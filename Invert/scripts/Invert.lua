3ac158a85d38eabf9833cfb08fdf7f5928d4d0b1
rdzanmi <mikolaj.rdzanek@sick.de>
2020-12-16 12:14:36 +0100

    'Update'

--- Invert/scripts/Invert.lua
+++ Invert/scripts/Invert.lua
@@ -27,7 +27,7 @@ print('AppEngine Version: ' .. Engine.getVersion())
 local DELAY = 2000 -- ms between each type for demonstration purpose
 
 -- Creating viewer
-local viewer = View.create()
+local viewer = View.create("viewer2D1")
 
 --End of Global Scope-----------------------------------------------------------
 
